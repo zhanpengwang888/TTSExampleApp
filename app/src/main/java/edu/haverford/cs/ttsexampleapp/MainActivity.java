14893a12f6aa83bc19b9a0f12ecd3832496b7b91
zhanpengwang888 <zhanpengwang888@gmail.com>
2018-12-12 01:05:55 -0500

    Example App finished.

--- app/src/main/java/edu/haverford/cs/ttsexampleapp/MainActivity.java
+++ app/src/main/java/edu/haverford/cs/ttsexampleapp/MainActivity.java
@@ -1,20 +1,33 @@
 package edu.haverford.cs.ttsexampleapp;
 
+import android.os.Build;
+import android.os.Environment;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    TextToSpeech mTextToSpeech;
+
+    protected TextToSpeech mTextToSpeech;
+    protected EditText editText;
+    protected String audioFolderPath;
+    protected boolean isSameLanguage = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audioFolderPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getPath();
         // initialization of TextToSpeech Instance
         mTextToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -29,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
                                 Toast.LENGTH_SHORT).show();
                     } else {
                         // it is supported
-                        int statusForPitch = mTextToSpeech.setPitch(2); // set the pitch to be normal
-                        int statusForRate = mTextToSpeech.setSpeechRate(2); // set the speech rate to 2
+                        int statusForPitch = mTextToSpeech.setPitch(1); // set the pitch to be normal
+                        int statusForRate = mTextToSpeech.setSpeechRate(1); // set the speech rate to 2
                         if (statusForPitch != TextToSpeech.SUCCESS) {
                             Toast.makeText(getApplicationContext(), "Fail to set pitch", Toast.LENGTH_SHORT).show();
                         }
@@ -41,5 +54,67 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.clearCheck();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton languageRadioButton = group.findViewById(checkedId);
+                if (languageRadioButton != null) {
+                    switch (languageRadioButton.getText().toString()) {
+                        case "English":
+                            mTextToSpeech.setLanguage(Locale.US);
+                            break;
+                        case "French":
+                            mTextToSpeech.setLanguage(Locale.FRANCE);
+                            break;
+                        case "Chinese":
+                            mTextToSpeech.setLanguage(Locale.CHINESE);
+                            break;
+                        case "Spanish":
+                            mTextToSpeech.setLanguage(new Locale("es"));
+                            break;
+                        default:
+                            mTextToSpeech.setLanguage(Locale.US);
+                            break;
+                    }
+                    isSameLanguage = false;
+                }
+            }
+        });
+
+        editText = findViewById(R.id.editText);
+        Button speechButton = findViewById(R.id.button);
+        speechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textForSpeech = editText.getText().toString();
+                String audioFileName = textForSpeech+".mp3";
+                String audioFileCompletePath = audioFolderPath + "/" + audioFileName.replaceAll("[^a-zA-Z0-9\\.\\-]", "");
+                File audioFile = new File(audioFileCompletePath);
+                if (audioFile.exists() && isSameLanguage) {
+                    mTextToSpeech.addSpeech(textForSpeech, audioFileCompletePath);
+                } else {
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        mTextToSpeech.synthesizeToFile(textForSpeech, null, audioFile, null);
+                    } else {
+                        mTextToSpeech.synthesizeToFile(textForSpeech, null, audioFileCompletePath);
+                    }
+                    isSameLanguage = true;
+                }
+                Toast.makeText(getApplicationContext(), textForSpeech, Toast.LENGTH_SHORT).show();
+                mTextToSpeech.speak(textForSpeech, TextToSpeech.QUEUE_ADD, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mTextToSpeech != null) {
+            mTextToSpeech.shutdown();
+        }
     }
 }
